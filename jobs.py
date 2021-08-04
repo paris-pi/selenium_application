57155b903b9d9dadea84e5cabfdcc32c118a4c5c
paris-pi <85985718+paris-pi@users.noreply.github.com>
2021-08-04 18:13:58 +0800

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,11 +1,13 @@
 import eight_btc_scraping
+import binance_scraping
 import threading
 
 
 
 if __name__ == '__main__':
     # 使用threading模块，threading.Thread()创建线程，其中target参数值为需要调用的方法，同样将其他多个线程放在一个列表中，遍历这个列表就能同时执行里面的函数了
-    threads = [threading.Thread(target=eight_btc_scraping.open_browser)]
+    threads = [threading.Thread(target=eight_btc_scraping.open_browser),
+               threading.Thread(target=binance_scraping.update,args=[binance_scraping.old_text_dict, 284, 522])]
     for t in threads:
         # 启动线程
         t.start()
