7761e052ba4ef009914cf5d99ac02468defff95d
paris-pi <85985718+paris-pi@users.noreply.github.com>
2021-08-04 18:05:27 +0800

    Add files via upload

---
+++ jobs.py
@@ -0,0 +1,11 @@
+import eight_btc_scraping
+import threading
+
+
+
+if __name__ == '__main__':
+    # 使用threading模块，threading.Thread()创建线程，其中target参数值为需要调用的方法，同样将其他多个线程放在一个列表中，遍历这个列表就能同时执行里面的函数了
+    threads = [threading.Thread(target=eight_btc_scraping.open_browser)]
+    for t in threads:
+        # 启动线程
+        t.start()
